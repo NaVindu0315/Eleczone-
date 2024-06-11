84fbb1b3f8d54453cb38a85e551989eb4a20e706
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-11 20:48:22 +0530

    total displaying done

--- Eleczone-/src/java/products/displaycart.java
+++ Eleczone-/src/java/products/displaycart.java
@@ -46,9 +46,9 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response)
         statement = connection.createStatement();
         resultSet = statement.executeQuery(sql);
 
-        out.println("<h2>Cart Contents:</h2>");
-        out.println("<table border='1'>");
-        out.println("<tr><th>ID</th><th>Name</th><th>Price</th><th>Count</th><th>Total</th></tr>");
+      /*  out.println("<h2>Shopping cart</h2>");
+        out.println("<table border='0'>");
+        out.println("<tr><td>Name</td><td>Price</td><td>Count</td><td>Total</td></tr>");
 
         while (resultSet.next()) {
             String id = resultSet.getString("id");
@@ -58,7 +58,7 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response)
             double total = resultSet.getDouble("total");
 
             out.println("<tr>");
-            out.println("<td>" + id + "</td>");
+            //out.println("<td>" + id + "</td>");
             out.println("<td>" + name + "</td>");
             out.println("<td>" + price + "</td>");
             out.println("<td>" + count + "</td>");
@@ -66,7 +66,35 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response)
             out.println("</tr>");
         }
 
-        out.println("</table>");
+        out.println("</table>");*/
+      out.println("<h2 style='text-align: center;'>Shopping cart</h2>");
+out.println("<table border='0' style='width: 50%; margin: 0 auto;'>");
+out.println("<tr><td style='width: 25%;'>Name</td><td style='width: 25%;'>Price</td><td style='width: 25%;'>Count</td><td style='width: 25%;'>Total</td></tr>");
+double totalAmount = 0;
+while (resultSet.next()) {
+    String id = resultSet.getString("id");
+    String name = resultSet.getString("name");
+    double price = resultSet.getDouble("price");
+    double count = resultSet.getDouble("count");
+    double total = resultSet.getDouble("total");
+
+    out.println("<tr>");
+    //out.println("<td>" + id + "</td>");
+    out.println("<td style='height: 30px;'>" + name + "</td>");
+    out.println("<td style='height: 30px;'>" + price + "</td>");
+    out.println("<td style='height: 30px;'>" + count + "</td>");
+    out.println("<td style='height: 30px;'>" + total + "</td>");
+    out.println("</tr>");
+   totalAmount += total;
+}
+
+out.println("</table>");
+out.println("<div align='center'>");
+out.println("<hr>");
+out.println("Total Amount = "+totalAmount);
+out.println("</div");
+out.print("<center><a href=\"dashboard.jsp\" class=\"button\">Dashboard</a></center>");
+out.print("<center><a href=\"products.jsp\" class=\"button\">Products</a></center>");
     } catch (Exception e) {
         out.println("<h3>Error: " + e.getMessage() + "</h3>");
     } finally {
